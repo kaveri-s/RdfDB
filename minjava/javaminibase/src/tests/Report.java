11c1cd07cb0db83c298f91a575865f9881677d96
Nishant Chaturvedi <nishant.kvd@gmail.com>
2022-03-20 23:17:45 -0700

    Added file read and writes to the report program

--- minjava/javaminibase/src/tests/Report.java
+++ minjava/javaminibase/src/tests/Report.java
@@ -1,5 +1,6 @@
 package tests;
 
+import diskmgr.PCounter;
 import global.*;
 
 import java.io.*;
@@ -17,6 +18,8 @@ public class Report
         SystemDefs sysdef = null;
         String dbname = null;   //Database name
         int indexoption = 0;    //Index option
+        int iread = PCounter.rCounter;
+        int iwrite = PCounter.wCounter;
         if(args.length == 2 )   //Check if the args are RDFDBNAME INDEXOPTION
         {
             try{
@@ -65,12 +68,12 @@ public class Report
             System.out.println(" DB Size 			: " + dbfile.length() + " bytes");
             System.out.println(" Page Size 			: " + SystemDefs.JavabaseDB.db_page_size() + " bytes");
             System.out.println(" Number of Pages in DB 		: " + SystemDefs.JavabaseDB.db_num_pages());
-            System.out.println(" Quadruple Size 			: " + GlobalConst.MINIBASE_QUADRUPLESIZE + " bytes");
+            System.out.println(" Quadruple Size 	        : " + GlobalConst.MINIBASE_QUADRUPLESIZE + " bytes");
             System.out.println(" Total Entities 		: " + SystemDefs.JavabaseDB.getEntityCnt());
             System.out.println(" Total Subjects 		: " + SystemDefs.JavabaseDB.getSubjectCnt());
             System.out.println(" Total Predicates 		: " + SystemDefs.JavabaseDB.getPredicateCnt());
             System.out.println(" Total Objects 			: " + SystemDefs.JavabaseDB.getObjectCnt());
-            System.out.println(" Total Quadruples			: " + SystemDefs.JavabaseDB.getQuadrupleCnt());
+            System.out.println(" Total Quadruples		: " + SystemDefs.JavabaseDB.getQuadrupleCnt());
             System.out.println(" Page Replacement Policy 	: Clock");
             System.out.println("\n --------- Heap Files ---------");
             System.out.println(" Quadruple File Name		: " + dbname + "/quadrupleHF");
@@ -79,6 +82,9 @@ public class Report
             System.out.println(" Entity File Record Count	: " + elhf.getLabelCnt());
             System.out.println(" Predicate File Name		: " + dbname + "/predicateHF");
             System.out.println(" Predicate File Record Count	: " + plhf.getLabelCnt());
+            System.out.println("\n --------- Page Read and Write  ---------");
+            System.out.println(" Total Page Reads               : " + String.valueOf(PCounter.rCounter) );
+            System.out.println(" Total Page Writes              : " + String.valueOf(PCounter.wCounter) );
             System.out.println(" ------------------------------");
             System.out.println("****************************************************************\n\n\n");
         }
